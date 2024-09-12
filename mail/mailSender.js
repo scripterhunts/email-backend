110958ece1e659aad21d46cb486ef7bacd544a3f
scripterhunts <108573998+scripterhunts@users.noreply.github.com>
2024-09-12 15:31:31 +0530

    Update mailSender.js

--- mail/mailSender.js
+++ mail/mailSender.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-// src/services/email.service.j
+// src/services/email.service.
 const nodemailer = require("nodemailer");
 
 const EMAIL_HOST = process.env.MAIL_HOST;
